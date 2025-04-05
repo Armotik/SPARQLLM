039332d29cab3419a3ef5292572b3f418bce9884
Pascal Molli <pascal.molli@ls2n.fr>
2025-04-05 11:03:50 +0200

    recurse partial

--- SPARQLLM/udf/recurse.py
+++ SPARQLLM/udf/recurse.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, Literal, URIRef
+from rdflib import BNode, Graph, Literal, URIRef
 from rdflib.namespace import XSD
 from rdflib.plugins.sparql import prepareQuery
 from rdflib.plugins.sparql.operators import register_custom_function
@@ -14,106 +14,58 @@ from SPARQLLM.udf.readdir import gettype, RDIR
 import os
 import json
 
-
 import logging
 import traceback
 
 from pathlib import Path
 
-rec_logger = logging.getLogger("recurse")
+logger = logging.getLogger(__name__)
 
-
-def recurse(query_str,gin,ginit,max_depth=10):
-    rec_logger.debug(f"RECURSE enter : {query_str}, gin: {gin},{type(gin)}, ginit: {ginit},{type(ginit)}")
-    all_graph_uri=URIRef("http://example.org/allg")    
-
-    ## Output
-    if  named_graph_exists(store, all_graph_uri):
-        rec_logger.debug(f"Recurse Graph {all_graph_uri} already exists (good)")
-        return None
-    else:
-        named_graph = store.get_context(all_graph_uri)
+def recurse(query_str,ginit,max_depth_lit=Literal(10)):
+    logger.debug(f"query:{query_str},  ginit:{ginit.n3()}, max_depth:{max_depth_lit}")
+    max_depth = max_depth_lit.value
 
     def func_recurse_on(gin_rec,depth=0):
-        print(f"RECURSE Recurse on : {gin_rec}")
-        result = store.query(query_str,initBindings={gin:gin_rec})
-        for row in result:
-            rec_logger.debug(f"REcurse row: {row}")
-            if row['gout'] == None:
-                rec_logger.debug(f"REcurse row: get None")
-                continue
+        logger.debug(f"Recurse on:{gin_rec}, size:{len(store.graph(gin_rec))}, depth:{depth}")
+#        for triple in store.graph(gin_rec):
+#            logger.debug(f"gin triple: {triple}")
+
+        # query the graph 
+        # If we want to use graphs in the construct query -> need a DS. (not a graph)      
+        result = store.graph(gin_rec).query(query_str)
+#        result = store.query(query_str) # dangerous -> all graphs
+#        result = store.query(query_str,initBindings={'?gin':gin_rec})
+        if (result.type!="CONSTRUCT"): 
+            logger.debug(f"recursive query is not a construct: {result}")
+            raise ValueError("result is not a graph : not a construct query")
+        if len(result) == 0:
+            logger.debug(f"result is empty")
+            return gin_rec
+        else:
+            gout=store.get_context(BNode())
+#            logger.debug(f"result: {result.graph} : {type(result.graph)}")
+            for s,p,o in result.graph:
+                gout.add((s,p,o))
+#            logger.debug(f"len(gin):{len(gin_rec)}, {len(store.graph(gin_rec))} len(gout):{len(gout)} ")
+            if len(gout) == len(store.graph(gin_rec)):
+                logger.debug(f"stop condition : {len(gout)} == {len(store.graph(gin_rec))}")
+                return gout.identifier
             else:
-                gout=URIRef(row['gout'])
-                print(f"RECURSE Recurse on : {gin_rec} -> {gout}")
-                named_graph.add((gin_rec, URIRef("http://example.org/has_graph"), gout))
-                named_graph.add((gout, URIRef("http://example.org/has_depth"), Literal(depth, datatype=XSD.integer)))
+#                logger.debug(f"depth {depth}, {type(depth)}, max_depth {max_depth} {type(max_depth)}")
                 if depth <= max_depth:
-                    func_recurse_on(gout,depth+1)
+                    return func_recurse_on(gout.identifier,depth+1)
                 else:
-                    rec_logger.debug(f"RECURSE max depth :  {max_depth} reached")
+                    logger.debug(f"max depth :  {max_depth} reached")
+                    return gout.identifier
 
     try:
-        func_recurse_on(ginit,0)
+        g_output = func_recurse_on(ginit,0)
     except Exception as e:
-        rec_logger.debug(f"RECURSE Exception {e}")
         traceback.print_exc()
+        raise ValueError("Recurse Error : "+str(e))
 
+    logger.debug(f"RECURSE end: graph {g_output} has {len(store.graph(g_output))} triples")
+#    for triple in g_output:
+#        logger.debug(f"recurse end triple: {triple}")
+    return g_output
 
-    # result=store.query(query_str,initBindings={'gin':ginit})   
-    # for row in result:
-    #     rec_logger.debug(f"Recurse: {row}")
-    #     named_graph.add((ginit, URIRef("http://example.org/has_graph"), row['gout']))
-
-    rec_logger.debug(f"RECURSE end: graph {all_graph_uri} has {len(all_graph_uri)} triples")
-    return all_graph_uri
-
-def testrec():
-
-    # SPARQL query using the custom function
-    query_str = """
-    PREFIX ex: <http://example.org/>
-    SELECT (MAX(?s) AS ?max_s)  WHERE {
-        ## init recursion
-        BIND(URI("file:///Users/molli-p/SPARQLLM") AS ?root)
-        BIND(ex:RDIR(?root,?root) AS ?ginit)
-        BIND(\"\"\"
-        PREFIX ex: <http://example.org/>
-        SELECT DISTINCT ?gout WHERE {
-            GRAPH ?gin {
-                ?root ex:has_path ?p1 .
-                ?p1 ex:has_type ?t1.
-                ?p1 ex:has_size ?s1 .
-                filter (str(?t1)="directory")
-            } 
-            BIND(ex:RDIR(?p1,?p1) as ?gout)
-            OPTIONAL {
-                GRAPH ?gout {
-                    ?p1 ex:has_path ?p2 .
-                    ?p2 ex:has_type ?t2.
-                    ?p2 ex:has_size ?s2 .
-                }
-            }
-        } \"\"\" AS ?query_str)
-        BIND(ex:RECURSE(?query_str,'?gin',?ginit,3) AS ?allg)
-        GRAPH ?allg {?init ex:has_graph ?g }
-        GRAPH ?g {?p ex:has_size ?s .}
-    }"""        
-
-    result = store.query(query_str)
-    for row in result:
-        for var in result.vars:  
-            print(f"{var}: {row[var]}")
-
-
-# run with python -m SPARQLLM.udf.recurse
-if __name__ == "__main__":
-    rec_logger = logging.getLogger("recurse")
-#    logging.basicConfig(level=logging.DEBUG)
-    rec_logger.setLevel(logging.DEBUG)
-
-    # Register the function with a custom URI
-    register_custom_function(URIRef("http://example.org/RECURSE"), recurse)
-    register_custom_function(URIRef("http://example.org/RDIR"), RDIR)
-
-
-    testrec()
